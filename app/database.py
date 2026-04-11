b0d6dd068e7be675ce29931f1960623064d0565d
MohamedWZS <mohamedwael.31.8.2000@gmail.com>
2026-04-11 23:38:12 +0200

    [MEL] Docker: add docker-compose + adjust DATABASE_URL connection string to use env variables

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql://mando@host.docker.internal:5432/podpilot"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
